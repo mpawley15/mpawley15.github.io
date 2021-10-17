3d8c6b3c13936f5732c5d638bfb5927fffa4149c
mpawley15 <75863144+mpawley15@users.noreply.github.com>
2021-10-17 19:44:51 -0400

    Update and rename assignments/assignment03.js to assignments/assignment03/assignment03.js

---
+++ assignments/assignment03/assignment03.js
@@ -0,0 +1,430 @@
+let elem = [];
+
+elem[1]
+  = document.getElementById("1");
+  
+elem[1].children[4].children[0].onclick 
+  = function(){
+	  add1(elem[1]);
+	  score(elem[1]);
+	  addTotalScore(elem[1]);
+	  };
+ 
+  elem[1].children[4].children[1].onclick 
+  = function(){
+	  remove1(elem[1]);
+	  score(elem[1]);
+	  addTotalScore(elem[1]);
+  };
+  elem[1].children[4].children[2].onclick 
+  = function(){
+	 clear(elem[1]); 
+	 addTotalScore(elem[1]);
+  }
+
+  elem[2]
+  = document.getElementById("2");
+  
+elem[2].children[4].children[0].onclick 
+  = function(){
+	  add1(elem[2]);
+	  score(elem[2]);
+	  addTotalScore(elem[2]);
+	  };
+ 
+  elem[2].children[4].children[1].onclick 
+  = function(){
+	  remove1(elem[2]);
+	  score(elem[2]);
+	  addTotalScore(elem[2]);
+  };
+    elem[2].children[4].children[2].onclick = function(){
+		clear(elem[2]);
+		addTotalScore(elem[2]);
+		}
+
+   elem[3]
+  = document.getElementById("3");
+  
+elem[3].children[4].children[0].onclick 
+ = function(){
+	  add1(elem[3]);
+	  score(elem[3]);
+	  addTotalScore(elem[3]);
+	  };
+  elem[3].children[4].children[1].onclick 
+  = function(){
+	  remove1(elem[3]);
+	  score(elem[3]);
+	  addTotalScore(elem[3]);
+  };
+    elem[3].children[4].children[2].onclick = function(){
+	 clear(elem[3]); 
+	 addTotalScore(elem[3]);
+  };
+
+   elem[4]
+  = document.getElementById("4");
+  
+elem[4].children[4].children[0].onclick 
+  = function(){
+	  add1(elem[4]);
+	  score(elem[4]);
+	  addTotalScore(elem[4]);
+	  };
+ 
+  elem[4].children[4].children[1].onclick = function(){
+	  remove1(elem[4]);
+	  score(elem[4]);
+	  addTotalScore(elem[4]);
+  };
+      elem[4].children[4].children[2].onclick = function(){
+	 clear(elem[4]); 
+	 addTotalScore(elem[4]);
+  };
+   elem[5] = document.getElementById("5");
+  
+elem[5].children[4].children[0].onclick = function(){
+	  add1(elem[5]);
+	  score(elem[5]);
+	  addTotalScore(elem[5]);
+	  };
+  elem[5].children[4].children[1].onclick = function(){
+	  remove1(elem[5]);
+	  score(elem[5]);
+	  addTotalScore(elem[5]);
+  };
+      elem[5].children[4].children[2].onclick  = function(){
+		  clear(elem[5]);
+			addTotalScore(elem[5]);		  
+  };
+  
+   elem[6]
+  = document.getElementById("6");
+  
+elem[6].children[4].children[0].onclick 
+ = function(){
+	  add1(elem[6]);
+	  score(elem[6]);
+	  addTotalScore(elem[6]);
+	  };
+	  
+  elem[6].children[4].children[1].onclick 
+  = function(){
+	  remove1(elem[6]);
+	  score(elem[6]);
+	  addTotalScore(elem[6]);
+  };
+  
+      elem[6].children[4].children[2].onclick 
+  = function(){
+	 clear(elem[6]); 
+	 addTotalScore(elem[6]);
+  };
+   elem[7]
+  = document.getElementById("7");
+  
+elem[7].children[4].children[0].onclick 
+= function(){
+	  add1(elem[7]);
+	  score(elem[7]);
+	  addTotalScore(elem[7]);
+	  };
+ 
+  elem[7].children[4].children[1].onclick 
+  = function(){
+	  remove1(elem[7]);
+	  score(elem[7]);
+	  addTotalScore(elem[7]);
+  };
+      elem[7].children[4].children[2].onclick 
+  = function(){
+	 clear(elem[7]);
+	 addTotalScore(elem[7]);	 
+  };
+   elem[8]
+  = document.getElementById("8");
+  
+elem[8].children[4].children[0].onclick 
+	= function(){
+	  add1(elem[8]);
+	  score(elem[8]);
+	  addTotalScore(elem[8]);
+	  };
+ 
+  elem[8].children[4].children[1].onclick 
+  = function(){
+	  remove1(elem[8]);
+	  score(elem[8]);
+	  addTotalScore(elem[8]);
+  };
+      elem[8].children[4].children[2].onclick 
+  = function(){
+	 clear(elem[8]); 
+	 addTotalScore(elem[8]);
+  }
+ elem[9]
+  = document.getElementById("9");
+  
+elem[9].children[4].children[0].onclick 
+  = function(){
+	  add1(elem[9]);
+	  score(elem[9]);
+	  addTotalScore(elem[9]);
+	  };
+ 
+  elem[9].children[4].children[1].onclick 
+  = function(){
+	  remove1(elem[9]);
+	  score(elem[9]);
+	  addTotalScore(elem[9]);
+  };
+      elem[9].children[4].children[2].onclick 
+  = function(){
+	 clear(elem[9]);
+     addTotalScore(elem[9]);	 
+  }
+  elem[10]
+  = document.getElementById("10");
+  
+elem[10].children[4].children[0].onclick 
+    = function(){
+	  add1(elem[10]);
+	  score(elem[10]);
+	  addTotalScore(elem[10]);	 
+	  };
+ 
+  elem[10].children[4].children[1].onclick 
+     = function(){
+	  remove1(elem[10]);
+	  score(elem[10]);
+	  addTotalScore(elem[11]);
+	  };
+	      elem[10].children[4].children[2].onclick 
+  = function(){
+	 clear(elem[10]); 
+	 addTotalScore(elem[10]);
+  };
+  elem[11]
+  = document.getElementById("11");
+  
+elem[11].children[4].children[0].onclick 
+     = function(){
+	  add1(elem[11]);
+	  score(elem[11]);
+	  addTotalScore(elem[11]);
+	  };
+  elem[11].children[4].children[1].onclick 
+  = function(){
+	  remove1(elem[11]);
+	  score(elem[11]);
+	  addTotalScore(elem[11]);
+  };
+  	      elem[11].children[4].children[2].onclick 
+  = function(){
+	 clear(elem[11]); 
+	 addTotalScore(elem[11]);
+  };
+  elem[12]
+  = document.getElementById("12");
+  
+elem[12].children[4].children[0].onclick 
+      = function(){
+	  add1(elem[12]);
+	  score(elem[12]);
+	  addTotalScore(elem[12]);
+	  };
+	  
+  elem[12].children[4].children[1].onclick 
+  = function(){
+	  remove1(elem[12]);
+	  score(elem[12]);
+	  addTotalScore(elem[12]);
+  };
+  	      elem[12].children[4].children[2].onclick 
+  = function(){
+	 clear(elem[12]); 
+	 addTotalScore(elem[12]);
+  };
+  elem[13]
+  = document.getElementById("13");
+  
+elem[13].children[4].children[0].onclick 
+     = function(){
+	  add1(elem[13]);
+	  score(elem[13]);
+	  addTotalScore(elem[13]);
+	  };
+  elem[13].children[4].children[1].onclick 
+  = function(){
+	  remove1(elem[13]);
+	  score(elem[13]);
+	  addTotalScore(elem[13]);
+  };
+  	      elem[13].children[4].children[2].onclick 
+  = function(){
+	 clear(elem[13]); 
+	 addTotalScore(elem[13]);
+  };
+  elem[14]
+  = document.getElementById("14");
+  
+elem[14].children[4].children[0].onclick 
+    = function(){
+	  add1(elem[14]);
+	  score(elem[14]);
+	  addTotalScore(elem[14]);
+	  };
+  elem[14].children[4].children[1].onclick 
+  = function(){
+	  remove1(elem[14]);
+	  score(elem[14]);
+	  addTotalScore(elem[14]);
+  };
+  	      elem[14].children[4].children[2].onclick 
+  = function(){
+	 clear(elem[14]);
+addTotalScore(elem[14]);	 
+  };
+  elem[15]
+  = document.getElementById("15");
+  
+elem[15].children[4].children[0].onclick 
+     = function(){
+	  add1(elem[15]);
+	  score(elem[15]);
+	  addTotalScore(elem[15]);
+	  };
+ 
+  elem[15].children[4].children[1].onclick 
+  = function(){
+	  remove1(elem[15]);
+	  score(elem[15]);
+	  addTotalScore(elem[15]);
+  };
+  	      	      elem[15].children[4].children[2].onclick 
+  = function(){
+	 clear(elem[15]); 
+	 addTotalScore(elem[15]);
+  }; 
+  elem[16]
+  = document.getElementById("16");
+  
+elem[16].children[4].children[0].onclick 
+    = function(){
+	  add1(elem[16]);
+	  score(elem[16]);
+	  addTotalScore(elem[16]);
+	  };
+  elem[16].children[4].children[1].onclick 
+  = function(){
+	  remove1(elem[16]);
+	  score(elem[16]);
+	  addTotalScore(elem[16]);
+  };
+  		      elem[16].children[4].children[2].onclick 
+  = function(){
+	 clear(elem[16]); 
+	 addTotalScore(elem[16]);
+  };
+  elem[17]
+  = document.getElementById("17");
+  
+elem[17].children[4].children[0].onclick 
+    = function(){
+	  add1(elem[17]);
+	  score(elem[17]);
+	  addTotalScore(elem[17]);
+	  };
+  elem[17].children[4].children[1].onclick 
+  = function(){
+	  remove1(elem[17]);
+	  score(elem[17]);
+	  addTotalScore(elem[17]);
+  };
+	      elem[17].children[4].children[2].onclick 
+  = function(){
+	 clear(elem[17]); 
+	 addTotalScore(elem[17]);
+  };
+  elem[18]
+  = document.getElementById("18");
+  
+elem[18].children[4].children[0].onclick 
+    = function(){
+	  add1(elem[18]);
+	  score(elem[18]);
+	  addTotalScore(elem[18]);
+	  };
+  elem[18].children[4].children[1].onclick 
+  = function(){
+	  remove1(elem[18]);
+	  score(elem[18]);
+	  addTotalScore(elem[18]);
+  };
+  	      elem[18].children[4].children[2].onclick 
+  = function(){
+	 clear(elem[18]); 
+	 addTotalScore(elem[18]);
+  };
+  elem[19] = document.getElementById("totals");
+  
+  
+// create an "add1" function
+function add1 (elem) {
+  if(elem.children[2].innerHTML == "-") {
+    elem.children[2].innerHTML = "1";
+	
+  }else {
+    let currentScore = elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+    elem.children[2].innerHTML = currentScore + 1;
+	
+  }
+}
+function remove1(elem){
+    let currentScore =elem.children[2].innerHTML;
+    currentScore = Number.parseInt(currentScore);
+	if(currentScore - 1 <=0){
+		elem.children[2].innerHTML = "-";
+	}else{
+		elem.children[2].innerHTML = currentScore - 1;
+		
+	}
+  
+}
+//function for overScore
+function score(elem){
+	let currentScore =elem.children[2].innerHTML;
+	const par = 4;
+	if(currentScore > 4){
+		let overScore = currentScore - par;
+		elem.children[3].innerHTML = overScore;
+	}else{
+		elem.children[3].innerHTML = "-";
+	}
+}
+function clear(elem){
+	elem.children[2].innerHTML = "-";
+	elem.children[3].innerHTML = "-";
+}
+
+function addTotalScore(){
+	let tPar = 0
+	let tScore = 0
+	let tOver = 0
+	for(let i = 1; i<=18; i++){
+		if (elem[i].children[1].innerHTML != "-"){
+            tPar += Number.parseInt(elem[i].children[1].innerHTML);
+		}
+        if (elem[i].children[2].innerHTML != "-"){
+            tScore += Number.parseInt(elem[i].children[2].innerHTML);
+		}
+        if (elem[i].children[3].innerHTML != "-"){
+            tOver += Number.parseInt(elem[i].children[3].innerHTML);
+		}
+	}
+		elem[19].children[1].innerHTML = tPar;
+		elem[19].children[2].innerHTML = tScore;
+		elem[19].children[3].innerHTML = tOver;	
+}
