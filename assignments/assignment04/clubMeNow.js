1ade8ed11b13348118e3b6f6441e0398794666ff
mpawley15 <75863144+mpawley15@users.noreply.github.com>
2021-10-20 16:28:29 -0400

    Update clubMeNow.js

--- assignments/assignment04/clubMeNow.js
+++ assignments/assignment04/clubMeNow.js
@@ -129,7 +129,7 @@ function resetAllClubDistances() {
 function displayAbout() {
 	// your code here
 	// window.location.href = "clubAbout.html";
-	window.alert("ClubMeNow version 1.0.0");
+	window.location.href = 'https://mpawley15.github.io/assignments/assignment04/clubAbout.html';
 }
 
 // navigate to "Penalty Info" screen
